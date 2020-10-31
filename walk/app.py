69bcf417d32c32dae4793c3502624c149ca6caa6
zoharsajith <zoharsajith@gmail.com>
2020-10-30 22:56:12 -0500

    wip

--- walk/app.py
+++ walk/app.py
@@ -18,30 +18,15 @@ def index():
 
     headers = {'User-Agent': 'Mozilla/5.0'}
 
-    number = 0
-
-    for i in range(0, 40):
-        if i < 40:
-            #move through list
-            search = nerves
-            article = []
-            results = 100 # valid options 10, 20, 30, 40, 50, and 100
-            page = requests.get(f"https://www.google.com/search?q={search}&num={results}&pws=0",headers = headers)
-            soup = BeautifulSoup(page.content, "html.parser")
-            links = soup.findAll("a")
-            for link in links :
-                link_href = link.get('href')
-                if "url?q=" in link_href and not "webcache" in link_href:
-                    article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
-
-            page = requests.get(f'{article[number]}', headers = headers)
-            soup = BeautifulSoup(page.text, 'html.parser')
-            text = soup.find('p').getText()
+    results = 1
+    page = requests.get(f"https://en.wikipedia.org/wiki/{search}")
+    soup = BeautifulSoup(page.content, "html.parser")
+    text = soup.text[1000:1500]
 
 
-        return render_template("index.html", text = text)
-        time.sleep(0.1)
-        i = i + 1
+
+
+    return render_template("index.html", text = text)
 
 @app.route('/nerves')
 
